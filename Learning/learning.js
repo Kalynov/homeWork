0c51c8e7091abc23703d83767090475f4e56140e
kalynov <kalynov@uchi.ru>
2018-06-07 10:40:49 +0300

    add tasks

--- Learning/learning.js
+++ Learning/learning.js
@@ -4,3 +4,40 @@ user.surname = "Petrov";
 user.name = "Sergey";
 delete user.name;
 console.log(user.surname.length);
+
+var schedule = {}
+//||||||||||||||||||||||||||||||||||||||||
+
+// Функция проверки наличия свойств у объета
+// возвращает true если объект пустой
+function isEmpty(obj) {
+   for (var key in obj){
+       console.log('в объекте есть свойства')
+        return(false);
+   }
+   console.log('объект пуст')
+   return(true);
+}
+  
+isEmpty(schedule);
+  
+schedule["8:30"] = "подъём";
+  
+isEmpty(schedule);
+//||||||||||||||||||||||||||||||||||||||||
+//Сумма Свойств\\
+
+var salaries = {
+    "Вася": 100,
+    "Петя": 300,
+    "Даша": 250
+  };
+
+function sumZP(obj){
+    var sum=0
+    for (var key in obj){
+        sum+=obj[key];
+    }
+    return (sum)
+}
+console.log(sumZP(salaries));
