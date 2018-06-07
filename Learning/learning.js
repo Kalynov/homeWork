40a036ab1f6f679514d13e90368bf084b37ef167
kalynov <kalynov@uchi.ru>
2018-06-07 15:59:05 +0300

    решето

--- Learning/learning.js
+++ Learning/learning.js
@@ -82,4 +82,52 @@ function multiplyNumeric(obj){
 
 multiplyNumeric(menu);
 
-console.log(menu);
+//console.log(menu);
+//||||||||||||||||||||||
+
+//Последний элемент.
+array = new Array(10);
+array[9]="last Element";
+function lastElement(arr){
+ //console.log(arr[arr.length-1]);
+}
+lastElement(array);
+
+
+//|||||||||||||||||||||||||||||||||||||||||||||\\
+// решето Эратосфена
+
+var allNumber = []
+for (var i =0; i<100; i++){
+    allNumber[i]= i;
+}
+var p;
+console.log(allNumber);
+function sieveEratosfen (arr) {
+    var p = 2;
+
+do {
+  // шаг 3
+  for (i = 2 * p; i < 100; i += p) {
+    arr[i] = false;
+  }
+
+  // шаг 4
+  for (i = p + 1; i < 100; i++) {
+    if (arr[i]) break;
+  }
+
+  p = i;
+} while (p * p < 100); // шаг 5
+
+// шаг 6 (готово)
+// посчитать сумму
+var sum = 0;
+for (i = 0; i < arr.length; i++) {
+  if (arr[i]) {
+    sum += i;
+    console.log(arr[i]);
+  }
+}
+}
+sieveEratosfen(allNumber);
