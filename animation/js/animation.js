e668187264101702f7a1251eb66ad0095b7f5263
kalynov <kalynov@uchi.ru>
2019-01-15 23:04:29 +0300

    fix animation

--- animation/js/animation.js
+++ animation/js/animation.js
@@ -1,16 +1,43 @@
-function myMove(){
+function myMove() {
     var elem = $("div .myAnimation");
     var pos = 0;
-    var id = setInterval(frame, 10);
-    function frame(){
-        if (pos==350){
+    var position = 0
+    var id = setInterval(frame, 5);
+    var flag = true;
+
+    function down() {
+        pos++;
+        position = position + 0.2;
+        flag = pos < 350 ? true : false;
+        elem.css({
+            top: pos,
+            left: position
+        });
+    }
+
+    function up() {
+        //var position = pos
+        //pos--;
+        pos--
+        position = position + 0.2;
+        flag = pos > 0 ? false : true;
+        console.log("up")
+        elem.css({
+            top: pos,
+            left: position
+        });
+    }
+
+
+    function frame() {
+        if (position > 350) {
             clearInterval(id);
-        }else{
-            pos++;
-            elem.css({
-                top: pos+'px',
-               // left: pos+'px'
-            });
+        } else {
+            if (pos < 350 && flag) {
+                down()
+            } else {
+                up()
+            }
         }
     }
 }
