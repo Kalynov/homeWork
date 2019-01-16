b9c213a6f49828c0b86e04d56355d24f91bb1d71
kalynov <kalynov@uchi.ru>
2019-01-17 01:23:54 +0300

    react learn

--- react/new_react_app/src/index.js
+++ react/new_react_app/src/index.js
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//ReactDOM.render( <App/> , document.getElementById('root'));
+//serviceWorker.unregister();
+function tic(){
+    let date = new Date().toLocaleTimeString();
+    const element =(
+        <div>
+            <h1>Hello</h1>
+            <h2>Time is {date}</h2>
+        </div>
+    );
+    console.log("!")
+    ReactDOM.render(element,document.getElementById('root')); 
+}
+setInterval(tic,1000)
